3f6933e3246ec64a0ca77a0b46b74c012f650c8b
juliosoftloper174-lab <226121092+juliosoftloper174-lab@users.noreply.github.com>
2026-06-03 10:12:12 -0500

    Commit: Funciona Todo hasta antes de mandarle datos de la base de datos y faltan agregar en farmacia en pedidos que si campos como preescriptor y diagostico llegue facio, pues que no se rellenen y pasen directo a agregar productos.

--- src/datos/test_data.py
+++ src/datos/test_data.py
@@ -5,99 +5,57 @@ MOVIMIENTOS = [
     {
         "tipo": "pedido",
         "data": {
-            "farmacia": "06732F02",
+            "farmacia": "06732F01",
             "cliente": "00033257",
             "prescriptor": "14571",
             "forma_pago": FormaPago.SIS,
             "tipo_receta": TipoReceta.SIN_NUMERO,
             "diagnosticos": [
-                "R100",
                 "R05X",
             ],
             "Medicamentos": [
                 {
-                    "codigo": "00091",
+                    "codigo": "30588",
                     "cantidad": 1,
-                },
-                {
-                    "codigo": "10155",
-                    "cantidad": 1,
-                },
+                }
             ],
             "fua": "786636652",
         },
     },
-    {
-        "tipo": "ingreso",
-        "data": {
-            "almacen_destino": "06732F01",
-            "almacen_virtual_origen": "030S0101",
-            "concepto": "DISTRIBUCION",
-            "medicamentos": [
-                {
-                    "codigo": "36394",
-                    "cantidad": 400,
-                    "lote": "LteNvo1",
-                    "tipo_ingreso": "SISMED-COMPRA NACIONAL (CN)",
-                    "documento": "Contribuciones a Fondos (CON)",
-                    "registro_sanitario": "SIN_REG_SAN",
-                    "fecha_vencimiento": "2029/12/20",
-                    "precio": "500",
-                }
-            ],
-        },
-    },
-    {
-        "tipo": "salida",
-        "data": {
-            "almacen_origen": "06732F01",
-            "almacen_destino": "06732F02",
-            "almacen_virtual_origen": "06732F0101",
-            "concepto": "DISTRIBUCION",
-            "medicamentos": [
-                {
-                    "codigo": "36394",
-                    "cantidad": 900,
-                    "lote": "DE5FDJ6D",
-                    "tipo": 1,
-                    "subtipo": 5,
-                }
-            ],
-        },
-    },
-    {
-        "tipo": "salida",
-        "data": {
-            "almacen_origen": "06732F01",
-            "almacen_destino": "06732F02",
-            "almacen_virtual_origen": "06732F0101",
-            "concepto": "DISTRIBUCION",
-            "medicamentos": [
-                {
-                    "codigo": "36394",
-                    "cantidad": 950,
-                    "lote": "DE5FDJ6D",
-                    "tipo": 1,
-                    "subtipo": 5,
-                }
-            ],
-        },
-    },
-    # INTERVENCION_SANITARIA ahora es válido (se eliminó ups_codigo)
     {
         "tipo": "pedido",
         "data": {
             "farmacia": "06732F01",
             "cliente": "00033257",
             "prescriptor": "14571",
-            "forma_pago": FormaPago.INTERVENCION_SANITARIA,
+            "forma_pago": FormaPago.SIS,
             "tipo_receta": TipoReceta.SIN_NUMERO,
             "diagnosticos": [
                 "R05X",
             ],
             "Medicamentos": [
                 {
-                    "codigo": "00091",
+                    "codigo": "30588",
+                    "cantidad": 1,
+                }
+            ],
+            "fua": "786636652",
+        },
+    },
+    {
+        "tipo": "pedido",
+        "data": {
+            "farmacia": "06732F01",
+            "cliente": "00033257",
+            "prescriptor": "14571",
+            "forma_pago": FormaPago.SIS,
+            "tipo_receta": TipoReceta.SIN_NUMERO,
+            "diagnosticos": [
+                "R05X",
+            ],
+            "Medicamentos": [
+                {
+                    "codigo": "30588",
                     "cantidad": 1,
                 }
             ],
@@ -113,7 +71,7 @@ MOVIMIENTOS = [
             "medicamentos": [
                 {
                     "codigo": "30588",
-                    "cantidad": 400,
+                    "cantidad": 1,
                     "lote": "LteNvo7",
                     "tipo_ingreso": "SISMED-COMPRA NACIONAL (CN)",
                     "documento": "Contribuciones a Fondos (CON)",
@@ -124,67 +82,22 @@ MOVIMIENTOS = [
             ],
         },
     },
-    # SIS sin FUA → EN REVISION (antes lanzaba excepción)
     {
-        "tipo": "pedido",
+        "tipo": "salida",
         "data": {
-            "farmacia": "06732F01",
-            "cliente": "00033257",
-            "prescriptor": "14571",
-            "forma_pago": FormaPago.SIS,
-            "tipo_receta": TipoReceta.SIN_NUMERO,
-            "diagnosticos": [
-                "R05X",
-            ],
-            "Medicamentos": [
+            "almacen_origen": "06732F01",
+            "almacen_destino": "06732F02",
+            "almacen_virtual_origen": "06732F0101",
+            "concepto": "DISTRIBUCION",
+            "medicamentos": [
                 {
-                    "codigo": "00091",
+                    "codigo": "30588",
                     "cantidad": 1,
+                    "lote": "LteNvo7",
+                    "tipo": 1,
+                    "subtipo": 5,
                 }
             ],
-            "fua": None,
-        },
-    },
-    # INTERVENCION_SANITARIA válido
-    {
-        "tipo": "pedido",
-        "data": {
-            "farmacia": "06732F01",
-            "cliente": "00033257",
-            "prescriptor": "14571",
-            "forma_pago": FormaPago.INTERVENCION_SANITARIA,
-            "tipo_receta": TipoReceta.SIN_NUMERO,
-            "diagnosticos": [
-                "R05X",
-            ],
-            "Medicamentos": [
-                {
-                    "codigo": "00091",
-                    "cantidad": 1,
-                }
-            ],
-            "fua": "786636652",
-        },
-    },
-    # tipo_receta != SIN_NUMERO → EN REVISION
-    {
-        "tipo": "pedido",
-        "data": {
-            "farmacia": "06732F01",
-            "cliente": "00033257",
-            "prescriptor": "14571",
-            "forma_pago": FormaPago.CONTADO,
-            "tipo_receta": TipoReceta.NUMERADA,
-            "diagnosticos": [
-                "R05X",
-            ],
-            "Medicamentos": [
-                {
-                    "codigo": "00091",
-                    "cantidad": 1,
-                }
-            ],
-            "fua": None,
         },
     },
 ]
